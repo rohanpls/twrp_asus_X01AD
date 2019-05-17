63be96acf9840e1b2c5ad70b392fe6bc276a7050
ashu7073 <ashu7073657271@gmail.com>
2019-05-17 19:03:22 +0530

    Changed common.mk path for pb

--- omni_X01AD.mk
+++ omni_X01AD.mk
@@ -29,7 +29,7 @@ PRODUCT_RELEASE_NAME := X01AD
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X01AD
